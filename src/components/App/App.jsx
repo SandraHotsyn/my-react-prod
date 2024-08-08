ca071a3ca2d76aa41dde36ed0beb55450d15cdba
Hotsyn <85835454+SandraHotsyn@users.noreply.github.com>
2024-08-08 17:15:43 +0300

    fix price

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,21 +7,21 @@ const prod1 = {
   name: "Tacos With Lime",
   imgUrl:
     "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
-  // price: {10.99},
+  price: 10.99,
 };
 
 const prod2 = {
   name: "Fries and Burger",
   imgUrl:
     "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640",
-  // price: {14.29},
+  price: 14.29,
 };
 
 const prod3 = {
   name: "Error",
   imgUrl:
     "https://upload.wikimedia.org/wikipedia/commons/0/02/Circle-icons-computer.svg",
-  // price:{0},
+  price: 0,
 };
 
 export default function App() {
