ea4b7fd7486168c594e7ba2baf5dc470954c16a2
Hotsyn <85835454+SandraHotsyn@users.noreply.github.com>
2024-08-08 13:57:03 +0300

    add prod

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,37 @@
 import { useState } from "react";
 import "./App.css";
 import Product from "../Product/Product";
+import Header from "../Header/Header";
+
+const prod1 = {
+  name: "Tacos With Lime",
+  imgUrl:
+    "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
+  // price: {10.99},
+};
+
+const prod2 = {
+  name: "Fries and Burger",
+  imgUrl:
+    "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640",
+  // price: {14.29},
+};
+
+const prod3 = {
+  name: "Error",
+  imgUrl:
+    "https://upload.wikimedia.org/wikipedia/commons/0/02/Circle-icons-computer.svg",
+  // price:{0},
+};
 
 export default function App() {
   return (
     <div>
+      <Header />
       <h1>Best selling</h1>
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-      <Product
-        name="Error"
-        imgUrl="https://upload.wikimedia.org/wikipedia/commons/0/02/Circle-icons-computer.svg"
-        price={0}
-      />
+      <Product prod={prod1} />
+      <Product prod={prod2} />
+      <Product prod={prod3} />
     </div>
   );
 }
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,11 @@
 // src/components/Product.jsx
 
-export default function Product(props) {
+export default function Product({ prod: { name, imgUrl, price } }) {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <img src={props.imgUrl} alt={props.name} width="480" />
-      <p>Price: {props.price} credits</p>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>
     </div>
   );
 }
