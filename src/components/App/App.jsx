330c4720e9e9ad5a5ef9217a9a8ce70c8a3fbe51
Hotsyn <85835454+SandraHotsyn@users.noreply.github.com>
2024-08-08 12:44:08 +0300

    add props

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
-import reactLogo from "../../assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Product from "../Product/Product";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
     <div>
-      <h1>Products</h1>
-
-      <Product />
-      <Product />
-      <Product />
+      <h1>Best selling</h1>
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+      <Product
+        name="Error"
+        imgUrl="https://upload.wikimedia.org/wikipedia/commons/0/02/Circle-icons-computer.svg"
+        price={0}
+      />
     </div>
   );
 }
-
-export default App;
